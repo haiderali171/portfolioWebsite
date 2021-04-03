90d466a3c089eca2f87243d4d66e1a0bb750dc81
haiderali171 <example_554@live.com>
2021-04-03 23:15:15 +0500

    settings change for seamless deployment

--- thesite/settings.py
+++ thesite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+django_heroku.settings(locals())
 import braintree
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
