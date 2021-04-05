4b4a0647dac37f7ed351346efc0b9d7639575426
haiderali171 <example_554@live.com>
2021-04-05 21:56:40 +0500

    Debug Settings

--- thesite/settings.py
+++ thesite/settings.py
@@ -220,3 +220,41 @@ BRAINTREE_CONF = braintree.Configuration( braintree.Environment.Sandbox, BRAINTR
 options = DATABASES['default'].get('OPTIONS', {})
 options.pop('sslmode', None)
 
+
+#Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
