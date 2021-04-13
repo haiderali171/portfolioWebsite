5a8d5a26368c56c6d3d30135e3e2b8fbcd9c3ab5
haiderali171 <example_554@live.com>
2021-04-13 20:21:46 +0500

    change something in the settings.py

--- thesite/settings.py
+++ thesite/settings.py
@@ -166,7 +166,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 #STATICFILES_DIRS = [
  #   os.path.join(BASE_DIR, 'static')
