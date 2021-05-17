05ca5115dff65abfba719e2e3545118e5046c6d6
haiderali171 <example_554@live.com>
2021-05-17 13:20:47 +0500

    Did something to requirements.txt

--- thesite/settings.py
+++ thesite/settings.py
@@ -26,7 +26,7 @@ env = environ.Env()
 
 environ.Env.read_env()
 
-
+# haha
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
