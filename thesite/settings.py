bff64af20f8b9b5a5fae1ebca00070cf5f224aab
haiderali171 <example_554@live.com>
2021-05-17 15:35:01 +0500

    Something to do with env

--- thesite/settings.py
+++ thesite/settings.py
@@ -27,6 +27,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # haha
+#
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -38,7 +39,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ['SECRET_KEY']
 
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
