fdbd7d824b0f45b8d061cce90b8628d32a5907bd
haiderali171 <example_554@live.com>
2021-10-28 00:41:53 +0500

    Gatsby Changes

---
+++ .cache/sync-requires.js
@@ -0,0 +1,10 @@
+
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": (preferDefault(require("/Users/Batman/Desktop/Portfolio Website/portfolioApp/thesite/.cache/dev-404-page.js")))
+}
+
